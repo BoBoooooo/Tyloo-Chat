64976feb3b99da14080cb75e9a8aed177eeb41ff
BoBoooooo <17746714@qq.com>
2020-12-02 17:34:01 +0800

    fix: formatBytes移除空格,防止windows下文件名出现空格报错

--- server/src/common/tool/utils.ts
+++ server/src/common/tool/utils.ts
@@ -46,5 +46,5 @@ export function formatBytes(bytes, decimals = 2) {
   const dm = decimals < 0 ? 0 : decimals
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i]
 }
