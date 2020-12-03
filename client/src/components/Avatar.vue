f89252779d48a53ab00429b66ae14d249f5a94eb
BoBoooooo <17746714@qq.com>
2020-12-03 16:11:29 +0800

    fix: 修复头像src bug

--- client/src/components/Avatar.vue
+++ client/src/components/Avatar.vue
@@ -11,7 +11,7 @@
         <a-card :bordered="false" style="width: 300px">
           <template slot="title">
             <h2>{{ userGather[data.userId].username }}</h2>
-            <a-avatar :size="60" style="float:right" :src="userGather[data.userId].avatar" />
+            <a-avatar :size="60" style="float:right" :src="userGather[data.userId].avatar || data.avatar" />
           </template>
           <a-button v-if="user.role === 'admin'" style="margin-bottom: 5px;" @click="deleteUser(data.userId)" type="primary">
             删除用户
